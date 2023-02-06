937d52a630e519f02306d81ab7ddbed03f209771
vivianw1 <viwu1@s.sfusd.edu>
2023-02-06 11:46:54 -0800

    Create a spawn manager + add animal prefabs in SM

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[ ] animalPrefabs; 
     // Start is called before the first frame update
     void Start()
     {
