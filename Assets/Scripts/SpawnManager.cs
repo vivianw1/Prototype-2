697309091b59caebf11d87a8f587cc19d719a08f
vivianw1 <viwu1@s.sfusd.edu>
2023-02-07 11:30:24 -0800

    Add collider and trigger components

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,13 @@ public class SpawnManager : MonoBehaviour
     public GameObject[ ] animalPrefabs; 
    private float spawnRangeX = 20; 
    private float spawnPosZ = 20;
+   private float startDelay = 2;
+   private float spawnInterval = 1.5f;
 
    // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal",startDelay,spawnInterval); 
     }
 
     // Update is called once per frame
@@ -19,7 +21,7 @@ public class SpawnManager : MonoBehaviour
     {
        if(Input.GetKeyDown(KeyCode.S))
        {
-       
+        SpawnRandomAnimal();
        } 
     } 
     
